35aa6af656a8167f8d68d641e77dcaa2efff7979
Badboss00 <111073451+Badboss00@users.noreply.github.com>
2022-08-11 17:31:36 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+let laender = ["China", "Mali", "Russia", "Slovakia"];
+
+for (let i = 0; i < laender.length; i++) {
+    console.log("- " + laender[i])  
+}
+*/
+
+var umfrageBtn = document.getElementById("umfrage");
+
+function myFunc() {
+    if ( umfrageBtn.innerHTML == "Umfrage") {
+        console.log("hi")
+    }
+}
+
